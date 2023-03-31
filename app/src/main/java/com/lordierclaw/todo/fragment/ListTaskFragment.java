d7ac7f1d9dc82d0f5db734eec238001b9c31bf05
Nguyen Nam Hai <lordierclaw@gmail.com>
2023-03-31 13:38:48 +0700

    Add background for empty list

--- app/src/main/java/com/lordierclaw/todo/fragment/ListTaskFragment.java
+++ app/src/main/java/com/lordierclaw/todo/fragment/ListTaskFragment.java
@@ -1,8 +1,8 @@
 package com.lordierclaw.todo.fragment;
 
+import androidx.core.widget.NestedScrollView;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,7 +11,6 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,8 +21,11 @@ import com.lordierclaw.todo.listener.ITaskLayoutListener;
 import com.lordierclaw.todo.model.Task;
 import com.lordierclaw.todo.viewmodel.ListTaskViewModel;
 
+import java.util.List;
+
 public class ListTaskFragment extends Fragment {
     private ListTaskViewModel mViewModel;
+    private RecyclerView tasksRcv;
     private TaskAdapter taskAdapter;
     private ITaskLayoutListener iTaskLayoutListener;
 
@@ -32,11 +34,11 @@ public class ListTaskFragment extends Fragment {
         View mView = inflater.inflate(R.layout.fragment_list_task, container, false);
         mViewModel = new ViewModelProvider(this).get(ListTaskViewModel.class);
         // Init UI and set Data
-        RecyclerView tasksRcv = mView.findViewById(R.id.tasks_rcv);
+        tasksRcv = mView.findViewById(R.id.tasks_rcv);
         taskAdapter = new TaskAdapter();
         taskAdapter.setTaskCheckBoxListener((task, isChecked) -> mViewModel.updateTaskChecked(task, isChecked));
         taskAdapter.setTaskLayoutListener(task -> iTaskLayoutListener.onClick(task));
-        mViewModel.getTaskList().observe(getViewLifecycleOwner(), list -> taskAdapter.submitList(list));
+        mViewModel.getTaskList().observe(getViewLifecycleOwner(), tasks -> submitList(tasks));
         tasksRcv.setLayoutManager(new LinearLayoutManager(getContext()));
         tasksRcv.setAdapter(taskAdapter);
         return mView;
@@ -45,16 +47,25 @@ public class ListTaskFragment extends Fragment {
     public void setTaskLayoutListener(ITaskLayoutListener listener) {
         iTaskLayoutListener = listener;
     }
+    private void submitList(List<Task> tasks) {
+        taskAdapter.submitList(tasks);
+        if (tasks.size() == 0) {
+            tasksRcv.setBackgroundResource(R.drawable.empty_list_bg);
+        } else {
+            tasksRcv.setBackgroundResource(0);
+        }
+    }
 
     public void setListType(Task.TaskGroup group) {
         if (mViewModel.getTaskList().hasObservers()) mViewModel.getTaskList().removeObservers(getViewLifecycleOwner());
         mViewModel.setTaskListByGroup(group);
-        mViewModel.getTaskList().observe(getViewLifecycleOwner(), list -> taskAdapter.submitList(list));
+        mViewModel.getTaskList().observe(getViewLifecycleOwner(), tasks -> submitList(tasks));
     }
+
 
     public void setListType() {
         if (mViewModel.getTaskList().hasObservers()) mViewModel.getTaskList().removeObservers(getViewLifecycleOwner());
         mViewModel.setTaskListMyDay();
-        mViewModel.getTaskList().observe(getViewLifecycleOwner(), list -> taskAdapter.submitList(list));
+        mViewModel.getTaskList().observe(getViewLifecycleOwner(), tasks -> submitList(tasks));
     }
 }
